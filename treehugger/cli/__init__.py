17ff99ac8c19d3615b8c4fc1e846c5aed21ca780
Adam Johnson <me@adamj.eu>
2017-03-31 11:56:03 +0100

    Allow specifying the KMS Key ID, Alias or ARN (#15)
    Fixes #7.
    
    Test Plan: Added tests. Locally tested cross-account key sharing for our internal use case.

--- treehugger/cli/__init__.py
+++ treehugger/cli/__init__.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import sys
 
+import six
+
+from ..kms import kms_agent
 from .decrypt_file import decrypt_file
 from .edit import edit
 from .encrypt_file import encrypt_file
@@ -15,6 +18,11 @@ def main(args_list=None):
     if args_list is None:
         args_list = sys.argv[1:]
     args = parser.parse_args(args_list)
+
+    # Global arguments
+    kms_agent.key_id = six.text_type(args.key_id)
+
+    # Call specific function
     func = command_funcs[args.command_name]
     func(args)
 
--- treehugger/cli/parser.py
+++ treehugger/cli/parser.py
@@ -2,13 +2,36 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
+import os
 
 from .. import __version__
+
+
+class VarOrDefault(object):
+    """
+    Helper to avoid fetching from environment variable until the moment it's
+    needed.
+    """
+    def __init__(self, env_var, default):
+        self.env_var = env_var
+        self.default = default
+
+    def __str__(self):
+        return os.environ.get(self.env_var, self.default)
+
 
 parser = argparse.ArgumentParser(
     prog='treehugger',
     description='''
         Takes care of the (runtime) environment. Version {version}.
     '''.format(version=__version__),
+)
+parser.add_argument(
+    '-k',
+    '--key',
+    type=str,
+    default=VarOrDefault('TREEHUGGER_KEY', 'alias/treehugger'),
+    dest='key_id',
+    help='The key ID, alias, or ARN to use on KMS for encryption.',
 )
 subparsers = parser.add_subparsers(dest='command_name')
