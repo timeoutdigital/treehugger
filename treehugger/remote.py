bb9e5af58ae1b349c6c6579b7046f6f874446d0b
Adam Johnson <me@adamj.eu>
2018-02-07 21:22:52 +0000

    Upgrade requirements

--- treehugger/remote.py
+++ treehugger/remote.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import boto3
 import six
 from botocore.exceptions import ClientError
-
 from six.moves.urllib.parse import parse_qs, urlparse
 
 from . import yaml
