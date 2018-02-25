f12a5314495b27215d8f10dc8f99a38718201ad5
Adam Johnson <me@adamj.eu>
2018-02-24 20:10:54 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'requests',
         'six',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license='ISC License',
     zip_safe=False,
     entry_points={
